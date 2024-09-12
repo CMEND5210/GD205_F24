5a01df2c86c6ddd10225cef92867a6f0c4372e7b
CMEND5210 <143760296+CMEND5210@users.noreply.github.com>
2024-09-12 16:02:34 -0400

    My current work so far.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            transform.position += new Vector3(1f, 0f, 0f);
+            Debug.Log("Congrats! You pressed D!");
+        }
+    //Moves the GameObject 1 unit positive along the X-axis when the "d" key is pressed.
+        if (Input.GetKeyDown("a"))
+        {
+            transform.position -= new Vector3(1f, 0f, 0f);
+            Debug.Log("Congrats! You pressed A!");
+        }
+    //Moves the GameObject 1 unit negative along the X-axis when the "a" key is pressed.
+        if (Input.GetKeyDown("e"))
+        {
+            transform.position += new Vector3(0f, 1f, 0f);
+            Debug.Log("Congrats! You pressed E!");
+        }
+    //Moves the GameObject 1 unit positive along the Y-axis when the "e" key is pressed.
+        if (Input.GetKeyDown("q"))
+        {
+            transform.position -= new Vector3(0f, 1f, 0f);
+            Debug.Log("Congrats! You pressed Q!");
+        }
+    //Moves the GameObject 1 unit negative along the Y-axis when the "q" key is pressed.
+        if (Input.GetKeyDown("w"))
+        {
+            transform.position += new Vector3(0f, 0f, 1f);
+            Debug.Log("Congrats! You pressed W!");
+        }
+    //Moves the GameObject 1 unit positive along the Z-axis when the "w" key is pressed.
+        if (Input.GetKeyDown("s"))
+        {
+            transform.position -= new Vector3(0f, 0f, 1f);
+            Debug.Log("Congrats! You pressed S!");
+        }
+    //Moves the GameObject 1 unit negative along the Z-axis when the "s" key is pressed.
+    }
+}
